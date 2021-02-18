f43ad4a68e3f499da609c6694d8817d741ecd19a
krivi <krivi95@hotmail.com>
2021-02-18 16:31:00 +0100

    HDWalletProvider: setting up smart contract deployment from MetaMask account

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,11 @@
 const path = require("path");
 
+// HDWalletProvider dependencies
+require("dotenv").config({path: ".env"});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic="";
+const accountIndex = 0;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -9,8 +15,14 @@ module.exports = {
       port: 7545,
       network_id: "5777",
       host: "127.0.0.1"
+    },
+    ganache_local: {
+      provider: function(){
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", accountIndex);
+      },
+      network_id: "5777",
     }
-  },
+  },  
   compilers: {
     solc: {
       version: "0.6.12"
