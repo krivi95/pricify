6d5a0e22c772a191e4ecd91ad11b391c4c0c96b6
krivi <krivi95@hotmail.com>
2021-08-07 09:45:37 +0200

    add table for leads from landing page

--- client/src/components/admindashboard/Messages.js
+++ client/src/components/admindashboard/Messages.js
@@ -1,19 +1,71 @@
 // ReactJS components
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //  Material-UI imports
-import { Box } from "@material-ui/core";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+
+// Firebase
+import firebase from "../../firebase/firebase";
+
+// Local ReactJS components
+import Title from "./Title";
+import Loading from "../Loading";
 
 function Messages() {
-  return (
-    <React.Fragment>
-      <Box display="flex" height={800}>
-        <Box m="auto">
-          <h2>Messages!!</h2>
-        </Box>
-      </Box>
-    </React.Fragment>
-  );
-}
+  const [leads, setLeads] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    async function getLeads() {
+        /**
+         * Getting the leas from database (messages from Landing Page)
+         */
+      var leadsRef = firebase.database().ref("leads");
+      var allLeads = Array();
+      await leadsRef.once("value", (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+          allLeads.push(childSnapshot.val());
+        });
+      });
+      setLeads(allLeads);
+      setIsLoading(false);
+    }
+
+    getLeads();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  } else {
+    return (
+      <React.Fragment>
+        <Title>Leads from landing page:</Title>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Message</TableCell>
+              <TableCell>Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {leads.map((row) => (
+              <TableRow key={row.date}>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.email}</TableCell>
+                <TableCell>{row.message}</TableCell>
+                <TableCell>{row.date}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </React.Fragment>
+    );
+  }
+}
 export default Messages;
