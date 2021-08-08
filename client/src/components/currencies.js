7b350a51fa2ca4566d3a7bd60d48cc0752e49a41
krivi <krivi95@hotmail.com>
2021-08-08 23:43:18 +0200

    finalize component for adding a new product

---
+++ client/src/components/currencies.js
@@ -0,0 +1,11 @@
+//ISO 4217 currencies
+// See list here: https://en.wikipedia.org/wiki/ISO_4217
+
+const isoCurrencyMapping = {
+    "USD": 840,
+    "EUR": 978,
+    "RSD": 941,
+    "JPY": 392
+}
+
+export default isoCurrencyMapping; 
--- client/src/components/storemanagerdashboard/NewProduct.js
+++ client/src/components/storemanagerdashboard/NewProduct.js
@@ -1,5 +1,5 @@
 // ReactJS components
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
 // MaterialUI components
 import TextField from "@material-ui/core/TextField";
@@ -8,6 +8,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 // Local ReactJS components
 import { default as CustomTypography } from "../landingpage/modules/components/Typography";
+import { SmartContractContext } from "../../context/SmartContractContext";
+import isoCurrencyMapping from "../currencies";
 
 //  Material-UI imports
 import { Box } from "@material-ui/core";
@@ -15,6 +17,9 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+
+// Firebase
+import firebase from "../../firebase/firebase";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -49,15 +54,76 @@ const currencyLabels = [
 
 function NewProduct() {
   const classes = useStyles();
+  const smartContractContext = useContext(SmartContractContext);
   const [isFormVisible, setIsFormVisibility] = useState(false);
+  const [inputData, setInputData] = React.useState({
+    name: "",
+    price: "",
+    currency: "USD",
+  });
   const [currency, setCurrency] = useState("EUR");
 
   const toggleForm = () => {
+    /**
+     * Shows/hides form for creating a new product
+     */
     setIsFormVisibility(!isFormVisible);
   };
 
-  const handleChange = (event) => {
-    setCurrency(event.target.value);
+  const createNewProduct = async () => {
+    /**
+     * Creates new product: rocord on blockchain and increasing counter in firebase db
+     */
+
+    if (!inputData.name || !inputData.currency) {
+      alert("Please enter a product name and price!");
+    }
+
+    // Add new product through StoreManager contract (addNewItemToMyStore method)
+    if (smartContractContext.contractInfo.storeManager) {
+      try {
+        await smartContractContext.contractInfo.storeManager.methods
+          .addNewItemToMyStore(
+            isoCurrencyMapping[inputData.currency],
+            inputData.name,
+            inputData.price
+          )
+          .send({ from: smartContractContext.contractInfo.accounts[0] });
+      } catch (error) {
+        console.log(error.message);
+        alert(error.message);
+        return;
+      }
+    } else {
+      alert(
+        "MetaMask hasn't been connected. Please conenct you're MetaMask wallet on home page and then continue"
+      );
+      return;
+    }
+
+    // Getting firebase Store Id from user's record
+    let firebaseStoreId = null;
+    var firebaseUserId = firebase.auth().currentUser.uid;
+    var userRef = firebase.database().ref("users/" + firebaseUserId);
+    await userRef.once("value", (snapshot) => {
+      firebaseStoreId = snapshot.val().storeId;
+    });
+
+    // Current number of store items
+    let numOfItems = null;
+    var storeRef = firebase.database().ref("stores/" + firebaseStoreId);
+    await storeRef.once("value", (snapshot) => {
+      numOfItems = snapshot.val().numOfItems;
+    });
+
+    // Updating store record in the Firebase database
+    storeRef = firebase.database().ref("stores").child(firebaseStoreId);
+    await storeRef.update({
+      numOfItems: numOfItems + 1,
+    });
+
+    console.log();
+    alert("Product " + inputData.name + "has been added to the blockchain.");
   };
 
   return (
@@ -99,27 +165,36 @@ function NewProduct() {
             <form className={classes.root}>
               <div>
                 <TextField
+                  name="name"
                   id="Name"
                   label="Name"
-                  defaultValue="Product name"
                   variant="outlined"
+                  onChange={(e) =>
+                    setInputData({ ...inputData, name: e.target.value })
+                  }
                 />
               </div>
               <div>
                 <TextField
+                  name="price"
                   id="Price"
                   label="Price"
-                  defaultValue="Price"
                   variant="outlined"
+                  onChange={(e) =>
+                    setInputData({ ...inputData, price: parseInt(e.target.value) })
+                  }
                 />
                 <TextField
+                  name="currency"
                   id="Currency"
                   select
                   label="Select"
-                  value={currency}
-                  onChange={handleChange}
+                  value="USD"
                   helperText="Please select your currency"
                   variant="outlined"
+                  onChange={(e) =>
+                    setInputData({ ...inputData, currency: e.target.value })
+                  }
                 >
                   {currencyLabels.map((option) => (
                     <MenuItem key={option.value} value={option.value}>
@@ -129,10 +204,10 @@ function NewProduct() {
                 </TextField>
               </div>
               <Button
-                type="submit"
                 color="secondary"
                 variant="contained"
                 className={classes.button}
+                onClick={createNewProduct}
               >
                 Add new product
               </Button>
